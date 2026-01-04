6539338d51f29045997a2fea2046ef8cba81b8c8
monicahcloud <monicahcloud@gmail.com>
2026-01-04 09:36:37 -0500

    adjust screen sizes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import ServicesSection from "./components/ServicesSection";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className=" ">
+    <div className="w-full">
+      <main className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
         <HeroSection />
         <ServicesSection />
         <About />
